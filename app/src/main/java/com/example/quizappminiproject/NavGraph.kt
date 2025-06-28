ad65d0c2e769e28d461245aa6e1077237a26e99c
Shivam Tawar <shivamtawar1804@gmail.com>
2025-06-28 21:11:58 +0530

    new updates

---
+++ app/src/main/java/com/example/quizappminiproject/NavGraph.kt
@@ -0,0 +1,27 @@
+package com.example.quizappminiproject
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph() {
+
+    val navContrller = rememberNavController()
+
+    NavHost(
+        navController = navContrller,
+        startDestination = "quiz"
+    ) {
+        composable("quiz"){
+            QuizScreen(navContrller)
+        }
+        composable("result/{score}"){ backStackEntry ->
+            val score = backStackEntry.arguments?.getString("score")?.toIntOrNull() ?:0
+            Score(score = score)
+        }
+    }
+
+
+}
